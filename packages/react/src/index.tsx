ed9b439c1c99f640b2d850ac1400d167a0ee4045
Charles-Mathieu Seguin <charles.mathieu.seguin@gmail.com>
2022-07-17 23:19:20 -0400

    feat: working version of useQuery & useMutation

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useEffect, useContext, useState, useRef } from "react";
 import {
+  MutationObservable,
   MutationParams,
   MutationResponse,
   NagareClient,
+  QueryObservable,
   QueryParams,
   QueryResponse,
 } from "@nagare/core";
@@ -23,40 +25,58 @@ export const NagareClientProvider: React.FC<{
 
 export function useQuery<T = unknown>(...args: QueryParams<T>) {
   const client = useContext(NagareContext);
+  const subscription = useRef<{ unsubscribe: () => void }>();
+  const queryObservable$ = useRef<QueryObservable<T>>();
 
   if (!client) {
     throw new Error("Client not defined");
   }
 
-  const queryObservable$ = client.query<T>(...args);
-  const [query, setQuery] = useState<QueryResponse<T>>();
+  if (!queryObservable$.current) {
+    queryObservable$.current = client.query<T>(...args);
+  }
+
+  const [query, setQuery] = useState<QueryResponse<T>>(
+    queryObservable$.current.initialResponse
+  );
 
   useEffect(() => {
-    const subscription = queryObservable$.subscribe((r) => setQuery(r));
+    subscription.current = queryObservable$.current?.subscribe((r) =>
+      setQuery(r)
+    );
     return () => {
-      subscription.unsubscribe();
+      subscription.current?.unsubscribe();
     };
   }, []);
 
-  return query;
+  return query as NonNullable<QueryResponse<T>>;
 }
 
 export function useMutation<T = unknown>(...args: MutationParams<T>) {
   const client = useContext(NagareContext);
+  const subscription = useRef<{ unsubscribe: () => void }>();
+  const mutationObservable$ = useRef<MutationObservable<T>>();
 
   if (!client) {
     throw new Error("Client not defined");
   }
 
-  const mutationObservable$ = client.mutation<T>(...args);
-  const [mutation, setMutation] = useState<MutationResponse<T>>();
+  if (!mutationObservable$.current) {
+    mutationObservable$.current = client.mutation<T>(...args);
+  }
+
+  const [mutation, setMutation] = useState<MutationResponse<T>>(
+    mutationObservable$.current.initialResponse
+  );
 
   useEffect(() => {
-    const subscription = mutationObservable$.subscribe((r) => setMutation(r));
+    subscription.current = mutationObservable$.current?.subscribe((r) =>
+      setMutation(r)
+    );
     return () => {
-      subscription.unsubscribe();
+      subscription.current?.unsubscribe();
     };
   }, []);
 
-  return mutation;
+  return mutation as NonNullable<MutationResponse<T>>;
 }
